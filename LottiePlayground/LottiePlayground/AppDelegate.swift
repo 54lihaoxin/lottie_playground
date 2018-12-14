d8507e0933693cd15a15d0064d1f05999c84aec9
Haoxin Li <54lihaoxin@gmail.com>
2018-12-14 14:12:43 -0800

    Project init

---
+++ LottiePlayground/LottiePlayground/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  LottiePlayground
+//
+//  Created by Haoxin Li on 12/14/18.
+//  Copyright © 2018 Haoxin Li. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
